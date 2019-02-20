f7fbb2b8a2a70476cb8e634a5c6a6c1950943ed0
daisenrong <daisenrong@guazi.com>
2019-02-20 10:26:58 +0800

    修改eureka注册中心为集群方式

--- eureka-server/src/main/java/com/lazydsr/springcloud/test/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/lazydsr/springcloud/test/eurekaserver/EurekaServerApplication.java
@@ -4,18 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        long l = System.currentTimeMillis();
-        System.out.println(l);
-        long l1 = l / 1000;
-        System.out.println(l1);
-        long l2 = l % 1000;
-        System.out.println(l2);
-        //SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
