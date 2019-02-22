d475735dcd427b49ee887dbc1bb8068ddf27a0b8
daisenrong <daisenrong@guazi.com>
2019-02-22 22:10:57 +0800

    配置注册中心集群

--- eureka-provider/src/main/java/com/lazydsr/springcloud/test/eurekaprovider/controller/HelloController.java
+++ eureka-provider/src/main/java/com/lazydsr/springcloud/test/eurekaprovider/controller/HelloController.java
@@ -1,9 +1,6 @@
 package com.lazydsr.springcloud.test.eurekaprovider.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
